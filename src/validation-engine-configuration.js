85023a8de2c41d7a7bb9553fa0099ec4408e7c77
Antonio Garrote <antoniogarrote@gmail.com>
2018-06-27 16:52:30 +0200

    Adding configuration object and allowing partial validation of constraints

---
+++ src/validation-engine-configuration.js
@@ -0,0 +1,17 @@
+
+var ValidationEngineConfiguration = function() {
+    // By default validate all errors
+    this.validationErrorBatch = -1;
+};
+
+
+ValidationEngineConfiguration.prototype.setValidationErrorBatch = function(validationErrorBatch) {
+    this.validationErrorBatch = validationErrorBatch;
+    return this;
+};
+
+ValidationEngineConfiguration.prototype.getValidationErrorBatch = function() {
+    return this.validationErrorBatch;
+};
+
+module.exports = ValidationEngineConfiguration;
---
+++ test/configuration_tests.js
@@ -0,0 +1,20 @@
+var SHACLValidator = require("../index");
+
+var fs = require("fs");
+
+exports.maxErrorsTest = function(test) {
+    var validator = new SHACLValidator();
+
+    var data = fs.readFileSync(__dirname + "/data/core/property/class-001.test.ttl").toString();
+
+    validator.validate(data, "text/turtle", data, "text/turtle", function(e, report) {
+        test.ok(report.conforms() === false);
+        test.ok(report.results().length === 2);
+        validator.getConfiguration().setValidationErrorBatch(1);
+        validator.validate(data, "text/turtle", data, "text/turtle", function(e, report) {
+            test.ok(report.conforms() === false);
+            test.ok(report.results().length === 1);
+            test.done();
+        });
+    });
+};
