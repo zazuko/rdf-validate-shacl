39cb62f4afe9b74989e69325c6894bf6de91fb25
Martin Maillard <self@martin-maillard.com>
2020-02-26 14:46:33 +0100

    Refactor ValidationFunction as a class

--- src/validation-function.js
+++ src/validation-function.js
@@ -4,69 +4,71 @@ var debug = require('debug')('validation-function')
 
 var globalObject = typeof window !== 'undefined' ? window : global
 
-var ValidationFunction = function (functionName, parameters, findInScript) {
-  this.funcName = functionName
-  this.func = findInScript(functionName)
-  if (!this.func) {
-    throw new Error('Cannot find validator function ' + functionName)
-  }
-  // Get list of argument of the function, see
-  // https://davidwalsh.name/javascript-arguments
-  var args = this.func.toString().match(/function\s.*?\(([^)]*)\)/)[1]
-  var funcArgsRaw = args.split(',').map(function (arg) {
-    return arg.replace(/\/\*.*\*\//, '').trim()
-  }).filter(function (arg) {
-    return arg
-  })
-  this.funcArgs = []
-  this.parameters = []
-  for (var i = 0; i < funcArgsRaw.length; i++) {
-    var arg = funcArgsRaw[i]
-    if (arg.indexOf('$') === 0) {
-      arg = arg.substring(1)
+class ValidationFunction {
+  constructor (functionName, parameters, findInScript) {
+    this.funcName = functionName
+    this.func = findInScript(functionName)
+    if (!this.func) {
+      throw new Error('Cannot find validator function ' + functionName)
     }
-    this.funcArgs.push(arg)
-    for (var j = 0; j < parameters.length; j++) {
-      var parameter = parameters[j]
-      var localName = RDFQuery.getLocalName(parameter.value)
-      if (arg === localName) {
-        this.parameters[i] = parameter
-        break
+    // Get list of argument of the function, see
+    // https://davidwalsh.name/javascript-arguments
+    var args = this.func.toString().match(/function\s.*?\(([^)]*)\)/)[1]
+    var funcArgsRaw = args.split(',').map(function (arg) {
+      return arg.replace(/\/\*.*\*\//, '').trim()
+    }).filter(function (arg) {
+      return arg
+    })
+    this.funcArgs = []
+    this.parameters = []
+    for (var i = 0; i < funcArgsRaw.length; i++) {
+      var arg = funcArgsRaw[i]
+      if (arg.indexOf('$') === 0) {
+        arg = arg.substring(1)
+      }
+      this.funcArgs.push(arg)
+      for (var j = 0; j < parameters.length; j++) {
+        var parameter = parameters[j]
+        var localName = RDFQuery.getLocalName(parameter.value)
+        if (arg === localName) {
+          this.parameters[i] = parameter
+          break
+        }
       }
     }
   }
-}
 
-ValidationFunction.prototype.doExecute = function (args) {
-  return this.func.apply(globalObject, args)
-}
-
-ValidationFunction.prototype.execute = function (focusNode, valueNode, constraint) {
-  debug('Validating ' + this.funcName)
-  var args = []
-  for (var i = 0; i < this.funcArgs.length; i++) {
-    var arg = this.funcArgs[i]
-    var param = this.parameters[i]
-    if (param) {
-      var value = constraint.getParameterValue(arg)
-      args.push(value)
-    } else if (arg === 'focusNode') {
-      args.push(focusNode)
-    } else if (arg === 'value') {
-      args.push(valueNode)
-    } else if (arg === 'currentShape') {
-      args.push(constraint.shape.shapeNode)
-    } else if (arg === 'path') {
-      args.push(constraint.shape.path)
-    } else if (arg === 'shapesGraph') {
-      args.push('DummyShapesGraph')
-    } else if (arg === 'this') {
-      args.push(focusNode)
-    } else {
-      throw new Error('Unexpected validator function argument ' + arg + ' for function ' + this.funcName)
-    }
+  doExecute (args) {
+    return this.func.apply(globalObject, args)
   }
-  return this.doExecute(args)
+
+  execute (focusNode, valueNode, constraint) {
+    debug('Validating ' + this.funcName)
+    var args = []
+    for (var i = 0; i < this.funcArgs.length; i++) {
+      var arg = this.funcArgs[i]
+      var param = this.parameters[i]
+      if (param) {
+        var value = constraint.getParameterValue(arg)
+        args.push(value)
+      } else if (arg === 'focusNode') {
+        args.push(focusNode)
+      } else if (arg === 'value') {
+        args.push(valueNode)
+      } else if (arg === 'currentShape') {
+        args.push(constraint.shape.shapeNode)
+      } else if (arg === 'path') {
+        args.push(constraint.shape.path)
+      } else if (arg === 'shapesGraph') {
+        args.push('DummyShapesGraph')
+      } else if (arg === 'this') {
+        args.push(focusNode)
+      } else {
+        throw new Error('Unexpected validator function argument ' + arg + ' for function ' + this.funcName)
+      }
+    }
+    return this.doExecute(args)
+  }
 }
 
 module.exports = ValidationFunction
