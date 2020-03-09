a476e9b97176c27116c02996f22f2e89732c63d4
Martin Maillard <self@martin-maillard.com>
2020-03-09 18:43:49 +0100

    Remove tests dependency on RDFLibGraph
    We can now treat RDFLibGraph as a purely internal detail and probably
    replace it slowly.

--- test/integration_tests.js
+++ test/integration_tests.js
@@ -5,75 +5,62 @@ const SHACLValidator = require('../index')
 const fs = require('fs')
 const rdf = require('rdf-ext')
 const rdfFS = require('rdf-utils-fs')
-// expected result
-const rdflibgraph = require('../src/rdflib-graph')
-const RDFLibGraph = rdflibgraph.RDFLibGraph
+const clownface = require('clownface')
+const { sh, rdf: rdfNS } = require('../src/namespaces')
 
-const ExpectedValidationResult = function (solution) {
-  this._id = solution.report.value
+class ExpectedValidationReport {
+  constructor (dataset) {
+    this.cf = clownface({ dataset })
+    this.reportNode = this.cf.namedNode(sh.ValidationReport).in(rdfNS.type)
+  }
 
-  this._focusNode = solution.focusNode.termType === 'BlankNode' ? '_:' + solution.focusNode.id : solution.focusNode.value
-  this._severity = solution.severity.value
-  this._constraint = solution.constraint.value
-  this._shape = solution.shape.termType === 'BlankNode' ? '_:' + solution.shape.id : solution.shape.value
-}
+  conforms () {
+    const conforms = this.reportNode.out(sh.conforms).value
+    return conforms === 'true'
+  }
 
-ExpectedValidationResult.prototype.id = function () {
-  return this._id
-}
-
-ExpectedValidationResult.prototype.focusNode = function () {
-  return this._focusNode
-}
-
-ExpectedValidationResult.prototype.severity = function () {
-  if (this._severity != null) {
-    return this._severity.split('#')[1]
+  results () {
+    return this.reportNode.out(sh.result).map((node) => new ExpectedValidationResult({
+      report: this.reportNode.term,
+      focusNode: node.out(sh.focusNode).term,
+      severity: node.out(sh.resultSeverity).term,
+      constraint: node.out(sh.sourceConstraintComponent).term,
+      shape: node.out(sh.sourceShape).term
+    }))
   }
 }
 
-ExpectedValidationResult.prototype.sourceConstraintComponent = function () {
-  return this._constraint
-}
+class ExpectedValidationResult {
+  constructor (solution) {
+    this._id = solution.report.value
 
-ExpectedValidationResult.prototype.sourceShape = function () {
-  return this._shape
-}
-
-const ExpectedValidationReport = function (graph) {
-  this.graph = graph
-}
-
-ExpectedValidationReport.prototype.conforms = function () {
-  const conforms = this.graph.query()
-    .match('?report', 'rdf:type', 'sh:ValidationReport')
-    .match('?report', 'sh:conforms', '?conforms')
-    .getNode('?conforms')
-  return conforms != null && conforms.value === 'true'
-}
-
-ExpectedValidationReport.prototype.results = function () {
-  const acc = []
-  const query = this.graph.query()
-    .match('?report', 'sh:result', '?result')
-    .match('?result', 'sh:focusNode', '?focusNode')
-    .match('?result', 'sh:resultSeverity', '?severity')
-    .match('?result', 'sh:sourceConstraintComponent', '?constraint')
-    .match('?result', 'sh:sourceShape', '?shape')
-  let solution = query.nextSolution()
-  while (solution != null) {
-    acc.push(new ExpectedValidationResult(solution))
-    solution = query.nextSolution()
+    this._focusNode = solution.focusNode.termType === 'BlankNode' ? '_:' + solution.focusNode.id : solution.focusNode.value
+    this._severity = solution.severity.value
+    this._constraint = solution.constraint.value
+    this._shape = solution.shape.termType === 'BlankNode' ? '_:' + solution.shape.id : solution.shape.value
   }
-  return acc
-}
 
-const expectedResult = async function (data, mediaType) {
-  const graph = new RDFLibGraph()
-  graph.loadGraph('http://test.com/example', data)
-  const expectedValidationReport = new ExpectedValidationReport(graph)
-  expectedValidationReport.results()
-  return expectedValidationReport
+  id () {
+    return this._id
+  }
+
+  focusNode () {
+    return this._focusNode
+  }
+
+  severity () {
+    if (this._severity != null) {
+      return this._severity.split('#')[1]
+    }
+  }
+
+  sourceConstraintComponent () {
+    return this._constraint
+  }
+
+  sourceShape () {
+    return this._shape
+  }
 }
 
 const isBlank = function (s) {
@@ -83,7 +70,7 @@ const isBlank = function (s) {
 const validateReports = async function (input) {
   const data = await rdf.dataset().import(rdfFS.fromFile(input))
 
-  const expectedReport = await expectedResult(data)
+  const expectedReport = new ExpectedValidationReport(data)
   const report = await new SHACLValidator().validate(data, data)
   assert.strictEqual(report.conforms(), expectedReport.conforms())
   assert.strictEqual(report.results().length, expectedReport.results().length)
