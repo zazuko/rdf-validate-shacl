zazuko/rdf-validate-shacl

commit fd78c936837ba77eac922a52f497b3b2d8e3c9b5

Remove constraint parameter values indirection

src/shapes-graph.js
@@ -21,7 +21,6 @@
 const NodeSet = require('./node-set')
 const ValidationFunction = require('./validation-function')
 const validatorsRegistry = require('./validators-registry')
-const { getLocalName } = require('./uri-utils')
 const { toRDFQueryPath } = require('./validators')
 const { rdfs, sh } = require('./namespaces')
 
@@ -107,21 +106,11 @@ class Constraint {
     this.shape = shape
     this.component = component
     this.paramValue = paramValue
-    const parameterValues = {}
-    const params = component.getParameters()
-    for (let i = 0; i < params.length; i++) {
-      const param = params[i]
-      const value = paramValue || rdfShapesGraph.cf.node(shape.shapeNode).out(param).term
-      if (value) {
-        const localName = getLocalName(param.value)
-        parameterValues[localName] = value
-      }
-    }
-    this.parameterValues = parameterValues
+    this.shapeNodeCf = rdfShapesGraph.cf.node(shape.shapeNode)
   }
 
-  getParameterValue (paramName) {
-    return this.parameterValues[paramName]
+  getParameterValue (param) {
+    return this.paramValue || this.shapeNodeCf.out(param).term
   }
 
   get componentMessages () {

src/uri-utils.js
@@ -1,16 +1,0 @@
-
-function getLocalName (uri) {
-  // TODO: This is not the 100% correct local name algorithm
-  let index = uri.lastIndexOf('#')
-  if (index < 0) {
-    index = uri.lastIndexOf('/')
-  }
-  if (index < 0) {
-    throw new Error('Cannot get local name of ' + uri)
-  }
-  return uri.substring(index + 1)
-}
-
-module.exports = {
-  getLocalName
-}

src/validators.js
@@ -19,19 +19,19 @@ const XSDDecimalTypes = new NodeSet([
 ])
 
 function validateAnd (context, focusNode, valueNode, constraint) {
-  const andNode = constraint.getParameterValue('and')
+  const andNode = constraint.getParameterValue(sh.and)
   const shapes = context.$shapes.rdfListToArray(andNode)
   return shapes.every((shape) => context.nodeConformsToShape(valueNode, shape))
 }
 
 function validateClass (context, focusNode, valueNode, constraint) {
-  const classNode = constraint.getParameterValue('class')
+  const classNode = constraint.getParameterValue(sh.class)
   return context.$data.isInstanceOf(valueNode, classNode)
 }
 
 function validateClosed (context, focusNode, valueNode, constraint) {
-  const closedNode = constraint.getParameterValue('closed')
-  const ignoredPropertiesNode = constraint.getParameterValue('ignoredProperties')
+  const closedNode = constraint.getParameterValue(sh.closed)
+  const ignoredPropertiesNode = constraint.getParameterValue(sh.ignoredProperties)
   const currentShape = constraint.shape.shapeNode
 
   if (!context.factory.true.equals(closedNode)) {
@@ -62,7 +62,7 @@ function validateClosed (context, focusNode, valueNode, constraint) {
 }
 
 function validateDatatype (context, focusNode, valueNode, constraint) {
-  const datatypeNode = constraint.getParameterValue('datatype')
+  const datatypeNode = constraint.getParameterValue(sh.datatype)
 
   if (valueNode.termType === 'Literal') {
     return datatypeNode.equals(valueNode.datatype) && isValidForDatatype(valueNode.value, datatypeNode)
@@ -72,13 +72,13 @@ function validateDatatype (context, focusNode, valueNode, constraint) {
 }
 
 function validateDisjoint (context, focusNode, valueNode, constraint) {
-  const disjointNode = constraint.getParameterValue('disjoint')
+  const disjointNode = constraint.getParameterValue(sh.disjoint)
   return !context.$data.hasMatch(focusNode, disjointNode, valueNode)
 }
 
 function validateEqualsProperty (context, focusNode, valueNode, constraint) {
   const pathNode = constraint.shape.path
-  const equalsNode = constraint.getParameterValue('equals')
+  const equalsNode = constraint.getParameterValue(sh.equals)
 
   const results = []
   const path = toRDFQueryPath(context.$shapes, pathNode)
@@ -100,7 +100,7 @@ function validateEqualsProperty (context, focusNode, valueNode, constraint) {
 }
 
 function validateEqualsNode (context, focusNode, valueNode, constraint) {
-  const equalsNode = constraint.getParameterValue('equals')
+  const equalsNode = constraint.getParameterValue(sh.equals)
   const results = []
 
   let solutions = 0
@@ -119,14 +119,14 @@ function validateEqualsNode (context, focusNode, valueNode, constraint) {
 }
 
 function validateHasValueNode (context, focusNode, valueNode, constraint) {
-  const hasValueNode = constraint.getParameterValue('hasValue')
+  const hasValueNode = constraint.getParameterValue(sh.hasValue)
   return focusNode.equals(hasValueNode)
 }
 
 function validateHasValueProperty (context, focusNode, valueNode, constraint) {
   const pathNode = constraint.shape.path
   const path = toRDFQueryPath(context.$shapes, pathNode)
-  const hasValueNode = constraint.getParameterValue('hasValue')
+  const hasValueNode = constraint.getParameterValue(sh.hasValue)
 
   return context.$data
     .getPathObjects(focusNode, path)
@@ -134,7 +134,7 @@ function validateHasValueProperty (context, focusNode, valueNode, constraint) {
 }
 
 function validateIn (context, focusNode, valueNode, constraint) {
-  const inNode = constraint.getParameterValue('in')
+  const inNode = constraint.getParameterValue(sh.in)
   return new NodeSet(context.$shapes.rdfListToArray(inNode)).has(valueNode)
 }
 
@@ -148,7 +148,7 @@ function validateLanguageIn (context, focusNode, valueNode, constraint) {
     return false
   }
 
-  const languageInNode = constraint.getParameterValue('languageIn')
+  const languageInNode = constraint.getParameterValue(sh.languageIn)
   const allowedLanguages = context.$shapes.rdfListToArray(languageInNode)
 
   return allowedLanguages.some(allowedLanguage => valueLanguage.startsWith(allowedLanguage.value))
@@ -158,7 +158,7 @@ function validateLessThanProperty (context, focusNode, valueNode, constraint) {
   const pathNode = constraint.shape.path
   const valuePath = toRDFQueryPath(context.$shapes, pathNode)
   const values = context.$data.getPathObjects(focusNode, valuePath)
-  const lessThanNode = constraint.getParameterValue('lessThan')
+  const lessThanNode = constraint.getParameterValue(sh.lessThan)
   const referenceValues = context.$data.cf.node(focusNode).out(lessThanNode).terms
 
   const invalidValues = []
@@ -177,7 +177,7 @@ function validateLessThanOrEqualsProperty (context, focusNode, valueNode, constr
   const pathNode = constraint.shape.path
   const valuePath = toRDFQueryPath(context.$shapes, pathNode)
   const values = context.$data.getPathObjects(focusNode, valuePath)
-  const lessThanOrEqualsNode = constraint.getParameterValue('lessThanOrEquals')
+  const lessThanOrEqualsNode = constraint.getParameterValue(sh.lessThanOrEquals)
   const referenceValues = context.$data.cf.node(focusNode).out(lessThanOrEqualsNode).terms
 
   const invalidValues = []
@@ -196,18 +196,18 @@ function validateMaxCountProperty (context, focusNode, valueNode, constraint) {
   const pathNode = constraint.shape.path
   const path = toRDFQueryPath(context.$shapes, pathNode)
   const count = context.$data.getPathObjects(focusNode, path).length
-  const maxCountNode = constraint.getParameterValue('maxCount')
+  const maxCountNode = constraint.getParameterValue(sh.maxCount)
 
   return count <= Number(maxCountNode.value)
 }
 
 function validateMaxExclusive (context, focusNode, valueNode, constraint) {
-  const maxExclusiveNode = constraint.getParameterValue('maxExclusive')
+  const maxExclusiveNode = constraint.getParameterValue(sh.maxExclusive)
   return valueNode.termType === 'Literal' && Number(valueNode.value) < Number(maxExclusiveNode.value)
 }
 
 function validateMaxInclusive (context, focusNode, valueNode, constraint) {
-  const maxInclusiveNode = constraint.getParameterValue('maxInclusive')
+  const maxInclusiveNode = constraint.getParameterValue(sh.maxInclusive)
   return valueNode.termType === 'Literal' && Number(valueNode.value) <= Number(maxInclusiveNode.value)
 }
 
@@ -216,7 +216,7 @@ function validateMaxLength (context, focusNode, valueNode, constraint) {
     return false
   }
 
-  const maxLengthNode = constraint.getParameterValue('maxLength')
+  const maxLengthNode = constraint.getParameterValue(sh.maxLength)
   return valueNode.value.length <= Number(maxLengthNode.value)
 }
 
@@ -224,18 +224,18 @@ function validateMinCountProperty (context, focusNode, valueNode, constraint) {
   const pathNode = constraint.shape.path
   const path = toRDFQueryPath(context.$shapes, pathNode)
   const count = context.$data.getPathObjects(focusNode, path).length
-  const minCountNode = constraint.getParameterValue('minCount')
+  const minCountNode = constraint.getParameterValue(sh.minCount)
 
   return count >= Number(minCountNode.value)
 }
 
 function validateMinExclusive (context, focusNode, valueNode, constraint) {
-  const minExclusiveNode = constraint.getParameterValue('minExclusive')
+  const minExclusiveNode = constraint.getParameterValue(sh.minExclusive)
   return valueNode.termType === 'Literal' && Number(valueNode.value) > Number(minExclusiveNode.value)
 }
 
 function validateMinInclusive (context, focusNode, valueNode, constraint) {
-  const minInclusiveNode = constraint.getParameterValue('minInclusive')
+  const minInclusiveNode = constraint.getParameterValue(sh.minInclusive)
   return valueNode.termType === 'Literal' && Number(valueNode.value) >= Number(minInclusiveNode.value)
 }
 
@@ -244,12 +244,12 @@ function validateMinLength (context, focusNode, valueNode, constraint) {
     return false
   }
 
-  const minLengthNode = constraint.getParameterValue('minLength')
+  const minLengthNode = constraint.getParameterValue(sh.minLength)
   return valueNode.value.length >= Number(minLengthNode.value)
 }
 
 function validateNodeKind (context, focusNode, valueNode, constraint) {
-  const nodeKindNode = constraint.getParameterValue('nodeKind')
+  const nodeKindNode = constraint.getParameterValue(sh.nodeKind)
 
   if (valueNode.termType === 'BlankNode') {
     return sh.BlankNode.equals(nodeKindNode) ||
@@ -267,17 +267,17 @@ function validateNodeKind (context, focusNode, valueNode, constraint) {
 }
 
 function validateNode (context, focusNode, valueNode, constraint) {
-  const nodeNode = constraint.getParameterValue('node')
+  const nodeNode = constraint.getParameterValue(sh.node)
   return context.nodeConformsToShape(valueNode, nodeNode)
 }
 
 function validateNot (context, focusNode, valueNode, constraint) {
-  const notNode = constraint.getParameterValue('not')
+  const notNode = constraint.getParameterValue(sh.not)
   return !context.nodeConformsToShape(valueNode, notNode)
 }
 
 function validateOr (context, focusNode, valueNode, constraint) {
-  const orNode = constraint.getParameterValue('or')
+  const orNode = constraint.getParameterValue(sh.or)
   const shapes = context.$shapes.rdfListToArray(orNode)
   return shapes.some(shape => context.nodeConformsToShape(valueNode, shape))
 }
@@ -287,30 +287,30 @@ function validatePattern (context, focusNode, valueNode, constraint) {
     return false
   }
 
-  const flagsNode = constraint.getParameterValue('flags')
-  const patternNode = constraint.getParameterValue('pattern')
+  const flagsNode = constraint.getParameterValue(sh.flags)
+  const patternNode = constraint.getParameterValue(sh.pattern)
   const re = flagsNode ? new RegExp(patternNode.value, flagsNode.value) : new RegExp(patternNode.value)
   return re.test(valueNode.value)
 }
 
 function validateQualifiedMaxCountProperty (context, focusNode, valueNode, constraint) {
   const count = validateQualifiedHelper(context, focusNode, constraint)
-  const qualifiedMaxCountNode = constraint.getParameterValue('qualifiedMaxCount')
+  const qualifiedMaxCountNode = constraint.getParameterValue(sh.qualifiedMaxCount)
 
   return qualifiedMaxCountNode.termType === 'Literal' && count <= Number(qualifiedMaxCountNode.value)
 }
 
 function validateQualifiedMinCountProperty (context, focusNode, valueNode, constraint) {
   const count = validateQualifiedHelper(context, focusNode, constraint)
-  const qualifiedMinCountNode = constraint.getParameterValue('qualifiedMinCount')
+  const qualifiedMinCountNode = constraint.getParameterValue(sh.qualifiedMinCount)
 
   return qualifiedMinCountNode.termType === 'Literal' && count >= Number(qualifiedMinCountNode.value)
 }
 
 function validateQualifiedHelper (context, focusNode, constraint) {
   const currentShapeNode = constraint.shape.shapeNode
-  const qualifiedValueShapesDisjointNode = constraint.getParameterValue('qualifiedValueShapesDisjoint')
-  const qualifiedValueShapeNode = constraint.getParameterValue('qualifiedValueShape')
+  const qualifiedValueShapesDisjointNode = constraint.getParameterValue(sh.qualifiedValueShapesDisjoint)
+  const qualifiedValueShapeNode = constraint.getParameterValue(sh.qualifiedValueShape)
 
   const siblingShapes = new NodeSet()
 
@@ -350,7 +350,7 @@ function validateQualifiedConformsToASibling (context, value, siblingShapes) {
 }
 
 function validateUniqueLangProperty (context, focusNode, valueNode, constraint) {
-  const uniqueLangNode = constraint.getParameterValue('uniqueLang')
+  const uniqueLangNode = constraint.getParameterValue(sh.uniqueLang)
 
   if (!context.factory.true.equals(uniqueLangNode)) {
     return
@@ -384,7 +384,7 @@ function validateUniqueLangProperty (context, focusNode, valueNode, constraint) 
 }
 
 function validateXone (context, focusNode, valueNode, constraint) {
-  const xoneNode = constraint.getParameterValue('xone')
+  const xoneNode = constraint.getParameterValue(sh.xone)
   const shapes = context.$shapes.rdfListToArray(xoneNode)
   const conformsCount = shapes
     .map(shape => context.nodeConformsToShape(valueNode, shape))

commit 2fa81292b60ddd1cf2e9817b792670ba9a9443ee

Fix validation messages variable substitution
Originally broken in fd78c93

src/shapes-graph.js
@@ -33,8 +33,7 @@ class ShapesGraph {
     // Build map from parameters to constraint components
     this.parametersMap = {}
     for (const component of this.components) {
-      const parameters = component.getParameters()
-      for (const parameter of parameters) {
+      for (const parameter of component.parameters) {
         this.parametersMap[parameter.value] = component
       }
     }
@@ -184,10 +183,6 @@ class ConstraintComponent {
     return validator || null
   }
 
-  getParameters () {
-    return this.parameters
-  }
-
   isComplete (shapeNode) {
     return !this.parameters.some((parameter) => (
       this.isRequired(parameter.value) &&
@@ -218,7 +213,7 @@ class Shape {
     shapeProperties.forEach((sol) => {
       const component = this.context.shapesGraph.getComponentWithParameter(sol.predicate)
       if (component && !handled.has(component.node)) {
-        const params = component.getParameters()
+        const params = component.parameters
         if (params.length === 1) {
           this.constraints.push(new Constraint(this, component, sol.object, context.$shapes))
         } else if (component.isComplete(shapeNode)) {

src/validation-engine.js
@@ -97,23 +97,26 @@ class ValidationEngine {
    * Creates a result message from the result and the message pattern in the constraint
    */
   createResultMessages (result, constraint) {
+    const $shapes = this.context.$shapes
     const { sh } = this.factory.ns
 
     // 1. Try to get message from the shape itself
-    let messages = [...this.context.$shapes
-      .match(constraint.shape.shapeNode, sh.message, null)]
-      .map(({ object }) => object)
+    let messages = $shapes.cf
+      .node(constraint.shape.shapeNode)
+      .out(sh.message)
+      .terms
 
     // 2. Try to get message from the constraint component validator
     if (messages.length === 0) {
       messages = constraint.componentMessages.map((m) => this.factory.literal(m))
     }
 
-    // 3. Try to get message from the constraint focus node
+    // 3. Try to get message from the constraint component node
     if (messages.length === 0) {
-      messages = [...this.context.$shapes
-        .match(constraint.component.node, sh.message, null)]
-        .map(({ object }) => object)
+      messages = $shapes.cf
+        .node(constraint.component.node)
+        .out(sh.message)
+        .terms
     }
 
     for (const message of messages) {
@@ -263,15 +266,16 @@ class ValidationEngine {
     }
   }
 
-  withSubstitutions (msg, constraint) {
-    let str = msg.value
-    const values = constraint.parameterValues
-    for (const key in values) {
-      const label = nodeLabel(values[key])
-      str = str.replace('{$' + key + '}', label)
-      str = str.replace('{?' + key + '}', label)
-    }
-    return this.factory.literal(str, msg.language | msg.datatype)
+  withSubstitutions (messageNode, constraint) {
+    const message = constraint.component.parameters.reduce((message, param) => {
+      const paramName = localName(param.value)
+      const paramValue = nodeLabel(constraint.getParameterValue(param))
+      return message
+        .replace(`{$${paramName}}`, paramValue)
+        .replace(`{?${paramName}}`, paramValue)
+    }, messageNode.value)
+
+    return this.factory.literal(message, messageNode.language | messageNode.datatype)
   }
 
   getReport () {
@@ -284,15 +288,36 @@ class ValidationEngine {
   }
 }
 
+// TODO: This is not the 100% correct local name algorithm
+function localName (uri) {
+  let index = uri.lastIndexOf('#')
+
+  if (index < 0) {
+    index = uri.lastIndexOf('/')
+  }
+
+  if (index < 0) {
+    throw new Error(`Cannot get local name of ${uri}`)
+  }
+
+  return uri.substring(index + 1)
+}
+
 function nodeLabel (node) {
+  if (!node) {
+    return 'NULL'
+  }
+
   if (node.termType === 'NamedNode') {
     // TODO: shrink URI if possible
     return '<' + node.value + '>'
-  } else if (node.termType === 'BlankNode') {
-    return 'Blank node ' + node.value
-  } else {
-    return '' + node.value
   }
+
+  if (node.termType === 'BlankNode') {
+    return 'Blank node ' + node.value
+  }
+
+  return node.value
 }
 
 module.exports = ValidationEngine

test/data/validation-message/message-empty.ttl
@@ -1,0 +1,12 @@
+@prefix sh: <http://www.w3.org/ns/shacl#> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+
+<Alice> a <Person> ;
+  <friend> <Bob> .
+
+<ShapeWithMessage> a sh:NodeShape ;
+  sh:targetClass <Person> ;
+  sh:property [
+    sh:path <friend> ;
+    sh:class <Person> ;
+  ] .

test/data/validation-message/message-from-constraint-component.ttl
@@ -1,0 +1,14 @@
+@prefix sh: <http://www.w3.org/ns/shacl#> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+
+<Alice> a <Person> ;
+  <friend> <Bob> .
+
+<ShapeWithMessage> a sh:NodeShape ;
+  sh:targetClass <Person> ;
+  sh:property [
+    sh:path <friend> ;
+    sh:class <Person> ;
+  ] .
+
+sh:ClassConstraintComponent sh:message "Message on constraint component" .

test/data/validation-message/message-from-shape-property.ttl
@@ -1,0 +1,15 @@
+@prefix sh: <http://www.w3.org/ns/shacl#> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+
+<Alice> a <Person> .
+
+<ShapeWithMessage> a sh:NodeShape ;
+  sh:targetClass <Person> ;
+  sh:property [
+    sh:path <name> ;
+    sh:datatype xsd:string ;
+    sh:minCount 1 ;
+    sh:message "My custom validation message" ;
+  ] .
+
+sh:MinCountConstraintComponent sh:message "Message on constraint component" .

test/data/validation-message/message-from-validator.ttl
@@ -1,0 +1,15 @@
+@prefix sh: <http://www.w3.org/ns/shacl#> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+
+<Alice> a <Person> ;
+  <name> "Alice" .
+
+<ShapeWithMessage> a sh:NodeShape ;
+  sh:targetClass <Person> ;
+  sh:closed true ;
+  sh:ignoredProperties (
+    rdf:type
+  ) .
+
+sh:ClosedConstraintComponent sh:message "Message on constraint component" .

test/data/validation-message/message-with-variable.ttl
@@ -1,0 +1,14 @@
+@prefix sh: <http://www.w3.org/ns/shacl#> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+
+<Alice> a <Person> .
+
+<ShapeWithMessage> a sh:NodeShape ;
+  sh:targetClass <Person> ;
+  sh:property [
+    sh:path <name> ;
+    sh:datatype xsd:string ;
+    sh:minCount 1 ;
+    sh:maxCount 2 ;
+    sh:message "Min count = {$minCount}" ;
+  ] .

test/validation_message_test.js
@@ -1,0 +1,70 @@
+/* eslint-env mocha */
+const path = require('path')
+const assert = require('assert')
+const $rdf = require('rdf-ext')
+const rdfFS = require('rdf-utils-fs')
+
+const SHACLValidator = require('../index')
+const rootPath = path.join(__dirname, '/data/validation-message')
+
+describe('validation messages', () => {
+  it('Returns message from shape property if provided', async () => {
+    const dataPath = path.join(rootPath, 'message-from-shape-property.ttl')
+    const data = await $rdf.dataset().import(rdfFS.fromFile(dataPath))
+    const shapes = data
+
+    const validator = new SHACLValidator(shapes)
+    const report = validator.validate(data)
+
+    assert.strictEqual(report.results.length, 1)
+    assert.strictEqual(report.results[0].message, 'My custom validation message')
+  })
+
+  it('Returns message from validator if provided (and no message on shape)', async () => {
+    const dataPath = path.join(rootPath, 'message-from-validator.ttl')
+    const data = await $rdf.dataset().import(rdfFS.fromFile(dataPath))
+    const shapes = data
+
+    const validator = new SHACLValidator(shapes)
+    const report = validator.validate(data)
+
+    assert.strictEqual(report.results.length, 1)
+    assert.strictEqual(report.results[0].message, 'Predicate is not allowed (closed shape)')
+  })
+
+  it('Returns message from constraint component if provided (and no message on shape or validator)', async () => {
+    const dataPath = path.join(rootPath, 'message-from-constraint-component.ttl')
+    const data = await $rdf.dataset().import(rdfFS.fromFile(dataPath))
+    const shapes = data
+
+    const validator = new SHACLValidator(shapes)
+    const report = validator.validate(data)
+
+    assert.strictEqual(report.results.length, 1)
+    assert.strictEqual(report.results[0].message, 'Message on constraint component')
+  })
+
+  it('Returns null if no message is defined anywhere', async () => {
+    const dataPath = path.join(rootPath, 'message-empty.ttl')
+    const data = await $rdf.dataset().import(rdfFS.fromFile(dataPath))
+    const shapes = data
+
+    const validator = new SHACLValidator(shapes)
+    const report = validator.validate(data)
+
+    assert.strictEqual(report.results.length, 1)
+    assert.strictEqual(report.results[0].message, null)
+  })
+
+  it('Substitutes variables in message', async () => {
+    const dataPath = path.join(rootPath, 'message-with-variable.ttl')
+    const data = await $rdf.dataset().import(rdfFS.fromFile(dataPath))
+    const shapes = data
+
+    const validator = new SHACLValidator(shapes)
+    const report = validator.validate(data)
+
+    assert.strictEqual(report.results.length, 1)
+    assert.strictEqual(report.results[0].message, 'Min count = 1')
+  })
+})