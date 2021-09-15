961578595c0e1c135cff8cc69ef485b253ad043c
Pieter Colpaert <pieter.colpaert@ugent.be>
2021-09-15 15:36:23 +0200

    Now also exports the property matching interface

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const clownface = require('clownface')
+const dataset = require('@rdfjs/dataset')
 const { prepareNamespaces } = require('./src/namespaces')
 const ShapesGraph = require('./src/shapes-graph')
 const ValidationEngine = require('./src/validation-engine')
 const shaclVocabularyFactory = require('./src/vocabularies/shacl')
+const rdf = require('rdf-ext')
 
 /**
  * Validates RDF data based on a set of RDF shapes.
@@ -75,6 +77,29 @@ class SHACLValidator {
       this.depth--
     }
   }
+}
+
+ /**
+* This exports 2 functions:
+*  - propertyPaths.extractPropertyPath
+*  - propertyPaths.getPathObjects
+*/
+SHACLValidator.propertyPaths = require('./src/property-path');
+
+/**
+* Public convenience function to take an array of triples instead of a Clownface interface 
+* 
+* Use as follows:
+*    validator.getPathObjectsFromQuads (quads, subject, validator.propertyPaths.extractPropertyPath)
+* 
+* @param {Quad[]} graph
+* @param {Term} subject - Start node
+* @param {object} path - Property path object
+* @return {Term[]} - Nodes that are reachable through the property path
+*/
+SHACLValidator.getPathObjectsFromQuads = function (quads, subject, path) {
+ const graph = clownface({ dataset: rdf.dataset(quads) })
+ return SHACLValidator.propertyPaths.getPathObjects(graph, subject, path)
 }
 
 module.exports = SHACLValidator
