3f77cc9edc8497450add691d358b78e5a2b7d814
Giacomo Citi <giacomo.citi@zazuko.com>
2024-04-11 09:32:47 +0200

    add test for result details

--- test/validation_details_test.js
+++ test/validation_details_test.js
@@ -7,12 +7,12 @@ import SHACLValidator from '../index.js'
 import { loadDataset } from './utils.js'
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
-const rootPath = path.join(__dirname, '/data/validation-repro')
+const rootPath = path.join(__dirname, '/data/validation-details')
 
-describe('validation repro', () => {
-  it('repro #125', async () => {
-    const data = await loadDataset(path.join(rootPath, 'repro125-data.ttl'))
-    const shapes = await loadDataset(path.join(rootPath, 'repro125-shapes.ttl'))
+describe('validation details', () => {
+  it('creates detail for node constraint', async () => {
+    const data = await loadDataset(path.join(rootPath, 'node-constraint-details.ttl'))
+    const shapes = data
 
     const validator = new SHACLValidator(shapes)
     const report = validator.validate(data)
@@ -21,15 +21,12 @@ describe('validation repro', () => {
     const result = report.results[0]
     assert.deepStrictEqual(result.sourceConstraintComponent, RDF.ns.sh.NodeConstraintComponent)
     assert.deepStrictEqual(result.focusNode, RDF.namedNode('https://example.org/person1'))
-    assert.deepStrictEqual(result.path, RDF.namedNode('https://example.org/address'))
-    assert.deepStrictEqual(result.value, RDF.namedNode('https://example.org/address1'))
-    assert.deepStrictEqual(result.message, [RDF.literal('ex:city should be sh:IRI')])
+    assert.deepStrictEqual(result.value, RDF.namedNode('https://example.org/person1'))
 
     assert.equal(1, result.detail.length)
     const detail = result.detail[0]
     assert.deepStrictEqual(detail.sourceConstraintComponent, RDF.ns.sh.NodeKindConstraintComponent)
-    assert.deepStrictEqual(detail.focusNode, RDF.namedNode('https://example.org/address1'))
-    assert.deepStrictEqual(detail.path, RDF.namedNode('https://example.org/city'))
-    assert.deepStrictEqual(detail.value, RDF.literal('London'))
+    assert.deepStrictEqual(detail.focusNode, RDF.namedNode('https://example.org/person1'))
+    assert.deepStrictEqual(detail.value, RDF.namedNode('https://example.org/person1'))
   })
 })
