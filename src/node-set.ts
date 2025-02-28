9831e7bb8c7bbf9598d3892cab2375d802e758ac
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2025-02-28 13:50:00 +0100

    revert: nodeset generic arg

--- src/node-set.ts
+++ src/node-set.ts
@@ -1,8 +1,8 @@
 import TermSet from '@rdfjs/term-set'
 import type { Term } from '@rdfjs/types'
 
-class NodeSet<T extends Term = Term> extends TermSet<T> {
-  addAll(nodes: Iterable<T>) {
+class NodeSet extends TermSet {
+  addAll(nodes: Iterable<Term>) {
     for (const node of nodes) {
       this.add(node)
     }
