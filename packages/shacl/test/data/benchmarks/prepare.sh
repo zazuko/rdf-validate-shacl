bf6c8319d984e6ae25cee8803aba66f8d8c8ca9d
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2025-07-30 13:55:37 +0200

    chore: move base library to packages (monorepo)

---
+++ packages/shacl/test/data/benchmarks/prepare.sh
@@ -0,0 +1,3 @@
+node ../../node_modules/@zazuko/shacl-test/load-graph.js \
+  https://cube.link/latest/shape/standalone-cube-constraint \
+  > test/data/benchmarks/cube-link/standalone-cube-constraint.ttl
